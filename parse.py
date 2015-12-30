72c544b69bb33b4f9987f1daf2e5fecf8705a43e
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2015-12-30 08:52:19 -0700

    Feed people data into dictionary

--- parse.py
+++ parse.py
@@ -1,9 +1,18 @@
 import csv
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
 
 people = {}
 
+def parse_csv_1_row(row, people):
+    person = {'first': row[1], 'last': row[2]}
+    people[row[0]] = person
+
 with open('peeps.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(csvreader) # Skip the first line
+    next(csvreader) # Skip the first line because it's a header
     for row in csvreader:
-        print ', '.join(row)
+        parse_csv_1_row(row, people)
+
+pp.pprint(people)
